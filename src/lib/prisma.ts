28f43b15642010fe7200dbc5fa8b6a5f16af171d
Renan <renanbelchior1@gmail.com>
2026-03-24 09:02:29 -0300

    Fix Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
+const dbUrl = process.env.DATABASE_URL || 'file:./dev.db'
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
 
-export default prisma
+const adapter = new PrismaBetterSqlite3({ url: dbUrl })
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
