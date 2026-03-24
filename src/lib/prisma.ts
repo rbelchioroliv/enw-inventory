171f2edc852ababfc7ce01566e3225727317700f
Renan <renanbelchior1@gmail.com>
2026-03-24 08:58:54 -0300

    Fix Prisma for Vercel deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-const dbUrl = process.env.DATABASE_URL || 'file:./dev.db'
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
-const adapter = new PrismaBetterSqlite3({ url: dbUrl })
+export default prisma
 
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
